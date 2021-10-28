0ed5099a17dcbcde28dc56f89ac076ebc70dcd63
mathar <mathar@mpia-hd.mpg.de>
2021-10-28 13:11:16 +0200

    mapping of fiber names to focal plane coords

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = 'Richard J. Mathar'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = " 0.0.278 "
+release = " 0.0.301 "
 
 
 # -- General configuration ---------------------------------------------------
--- fieldrotation/HomTrans.py
+++ fieldrotation/HomTrans.py
@@ -12,6 +12,7 @@ import numpy
 import astropy.coordinates
 import astropy.time
 import astropy.units
+import Fiber
 
 __all__ = ['HomTrans', 'Mirr', 'Site', 'Ambi', 'Target', 'Sider']
 
@@ -152,8 +153,10 @@ class Ambi():
         self.wlen = wlen
         if press is None :
             if site is None :
+                # no information whatsoever: assume sea level
                 self.press = 1013.0
             elif isinstance(site, Site) :
+                # assume 8.1 km scale height
                 self.press = 1013.0*math.exp(-site.alt/8135.0)
             else :
                 self.press = 1013.0
@@ -165,7 +168,7 @@ class Target():
     """
     def __init__(self, targ) :
         """ target coordinates
-        :param targ Position in ecliptic coordinates
+        :param targ Position in equatorial coordinates
         :type targ astropy.coordinates.Skycoord
         """
 
@@ -176,9 +179,9 @@ class Target():
         # print(self.targ)
 
     def toHoriz(self, site, ambi = None, time = None) :
-        """ convert from elliptical to horizontal coordinates
+        """ convert from equatorial to horizontal coordinates
         :param site Observatory location
-        :type site fieldrotation.site
+        :type site fieldrotation.Site
         :param ambi Ambient parameters characterizing refraction
         :type ambi fieldrotation.Ambi
         :param time time of the observation
@@ -274,7 +277,7 @@ class Sider():
         :type ambi
         :param wlen wavelenght of observation in microns
         :type wlen float
-        :param time time of the observation /UTC
+        :param time time of the observation /UTC; if None, the current time will be used.
         :type time
         :return field angle (direction to NCP) in radians
         """
@@ -294,65 +297,153 @@ class Sider():
         star[0] = math.sin( horiz.az.radian) * math.cos( horiz.alt.radian )
         star[1] = math.cos( horiz.az.radian ) * math.cos( horiz.alt.radian)
         star[2] = math.sin( horiz.alt.radian)
-        # print("star",star)
 
+        # unit vector from M2 to M1
         m2tom1 = numpy.cross(star,self.b)
         len = numpy.linalg.norm(m2tom1)
         m2tom1 /= self.sign*len
 
+        # surface normal to M1
         m1norm = star - m2tom1
         len = numpy.linalg.norm(m1norm)
         m1norm /= len
         m1 = Mirr(m1norm,1.0)
 
+        # surface normal to M2
         m2norm = self.b + m2tom1
         len = numpy.linalg.norm(m2norm)
         m2norm /= len
         m2 = Mirr(m2norm,0.0)
 
+        # transformation matrix for the 2 reflections
         m1trans = m1.toHomTrans()
         m2trans = m2.toHomTrans()
         trans = m2trans.multiply(m1trans)
 
         # for the field angle need a target that is just a little bit
         # more north (but not too little to avoid loss of precision)
-        # print('target',target.targ)
-	# 10 arcmin = 0.16 deg further N
+	# 10 arcmin = 0.16 deg further to NCP
         targNcp = Target(target.targ.spherical_offsets_by(
                        astropy.coordinates.Angle("0deg"), astropy.coordinates.Angle("0.16deg")))
-        # print('targetN',targNcp.targ)
         horizNcp = targNcp.toHoriz(site=site, ambi=ambi, time = now) 
-        # print('targetN atlatz',horizNcp)
 
         starNcp = numpy.zeros((3))
         # same procedure as in the construction of b in the Sider ctor, but with 90-zenang=alt
         starNcp[0] = math.sin( horizNcp.az.radian) * math.cos( horizNcp.alt.radian )
         starNcp[1] = math.cos( horizNcp.az.radian ) * math.cos( horizNcp.alt.radian)
         starNcp[2] = math.sin( horizNcp.alt.radian)
-        # print("starNcp",starNcp)
 
         # image of targNcp while hitting M1
         m1img = trans.apply(m2tom1)
-        # print("m1img",m1img)
         # image of targNcp before arriving at M1
         starOffm1 = m2tom1 + starNcp
         starimg = trans.apply(starOffm1)
-        # print("starimg",starimg)
 
         # virtual direction of ray as seen from point after M2
         # no need to normalize this because the atan2 will do...
         starvirt = m1img - starimg 
-        # print("diff img",starvirt)
 
         # project in a plane orthogonal to  self.b
         cosFang = numpy.dot(starvirt,self.box)
         sinFang = numpy.dot(starvirt,self.boy)
         return math.atan2(sinFang, cosFang)
 
+    def centrTarg(self, site, target, ambi, fib, flen = 1839.8, wlen=0.5, time=None) :
+        """ 
+        Compute a target (in the usual astrometic sense) by assuming
+        the astronomer wants to place the target of the argument list on
+        the head of the fiber in the argument list.
+        This covers the generic task to let the telescope acquire a
+        target that is supposed to land on one of the P1-x or P2-x fiber
+        heads of the spectophotometric channel.
+         
+
+        This involves basically calculating the current direction
+        to the NCP at the nominal target in the focal plane, calculating
+        the position of the fiber head in the focal plane, and using
+        the center of the fiber bundle as a distance and position
+        angle relativ to the nominal target...
+        :param site location of the observatory
+        :type site fieldrotation.Site
+        :param target sidereal target in ra/dec
+        :type target astropy.coordinates
+        :param ambi Ambient data relevant for refractive index
+        :type ambi
+        :param fib One of the fibers in one of the 4 tables
+        :type Fiber
+        :param flen Focal length (defining th eplate scale) in mm
+                    This should be the same as in the YAML file of the cameras...
+        :type float
+        :param wlen wavelenght of observation in microns
+        :type wlen float
+        :param time time of the observation /UTC; if None, the current time will be used.
+        :type time
+        :return a new target which is in the fiber bundle center when target is at the fiber
+        """
+
+        # direction to NCP from target in the FP orientation (=0 if NCP=up)
+        fang  = self.fieldAngle(site, target, ambi, wlen, time)
+
+        # direction of the fiber relative to the bundle center
+        labang = fib.labAngle()
+        # coordinates of fiber in microns relative to bundle center
+        xfib, yfib = fib.xyFocalPlane() 
+
+        # plate scale, microns in the focal plane per radian on the sky
+        # image scale at focus is 8.92 um/arcsec = 8.92um/(pi/180/3600 rad) =1.8e^6 um/rad
+        pscal = 1000.*flen
+
+        # distance center to fiber in radians on the sky
+        throwrad = math.hypot( xfib, yfib)/pscal
+
+        # The lab angle of the fiber location to the fiber bundle center
+        # (again up=0) is essentially 180 degress plus the labang
+        # (quasi by inversion of the role of the coordinate systems of the two)
+        # The angle from "up" to NCP is fang, and the angle from "up" to center
+        # is 180+labang, so the angle from NCP (clockwise) to center is
+        # 180+laban-fang. The position angle on the sky is defined c.c.w. 
+        # Whether "clockwise" means a position angle
+        # (for flipped images) or the negative position angle (for non-flipped)
+        # depends on a K-mirror being present or not. The non-flipped 
+        # images are on the spectrophoom (names P*) the flipped on
+        # the science and background (name S*, A*, B*)
+        posang = fang -labang -math.pi
+        if fib.name[0:1] != 'P' :
+            posang *= -1
+
+        # we do not care her to wrap this into [-180..180] or [0..360] or whatever
+
+        # print("field angle target " + str(math.degrees(fang)))
+        # print("lab angle fiber " + str(math.degrees(labang)))
+        # print("position angle ctr " + str(math.degrees(posang)))
+        # print("throw " + str(3600*math.degrees(throwrad)) + "arcsec")
+
+        # apply offset to the given target
+        targCtr = Target(target.targ.directional_offset_by(
+                       posang *astropy.units.rad, throwrad*astropy.units.rad))
+
+        return targCtr
+
+    def mpiaMocon(self, site, target, ambi, wlen=0.5, time=None) :
+        """ 
+        :param site location of the observatory
+        :type site fieldrotation.Site
+        :param target sidereal target in ra/dec
+        :type target astropy.coordinates
+        :param ambi Ambient data relevant for refractive index
+        :type ambi
+        :param wlen wavelenght of observation in microns
+        :type wlen float
+        :param time time of the observation /UTC; if None, the current time will be used.
+        :type time
+        :return field angle (direction to NCP) in radians
+        """
+        pass
+
 
 if __name__ == "__main__":
     """ Example application demonstrating the interface.
-    
+    .. todo demonstrate use of proper motions 
     """
     import argparse
     parser = argparse.ArgumentParser()
@@ -368,9 +459,12 @@ if __name__ == "__main__":
     # shortcut for site coordinates: observatory
     parser.add_argument("-s", '--site', default="LCO", help="LCO or MPIA or APO or KHU")
 
+    # optional name of a fiber
+    parser.add_argument("-f", '--fiber', help="fiber name like P2-1 or P1-11")
+
     args = parser.parse_args()
 
-    # check ranges and combine ra/dec into a single SkyCoord
+    # check ranges and combine ra/dec into a astropy SkyCoord
     if args.ra is not None and args.dec is not None :
         if args.ra.find("h") < 0 :
             # apparently simple floating point representation
@@ -395,3 +489,11 @@ if __name__ == "__main__":
     # calculate the field angle (in radians)
     rads = sid.fieldAngle(geoloc, point, None)
     print("field angle " + str(math.degrees(rads)) + " deg")
+
+    # if a P[12]-[1..12] fiber head was specified, calculate
+    # also the virtual target in the fiber bundle center.
+    if args.fiber is not None and targ is not None :
+        fib=Fiber.Fiber(args.fiber)
+        # print("lab angle " + str(math.degrees(fib.labAngle())) + " deg")
+        ctrTarg = sid.centrTarg(geoloc, point, None, fib)
+        print(ctrTarg.targ)
